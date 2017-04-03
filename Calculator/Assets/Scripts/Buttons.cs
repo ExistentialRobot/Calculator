3b51e8b9a96462143b3282bd4a4d80a555c18191
Unknown <mistergroo25@gmail.com>
2017-04-03 11:07:13 -0600

    Started Calculator Project

---
+++ Calculator/Assets/Scripts/Buttons.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Buttons : MonoBehaviour {
+
+    public MathOperators math;
+    public InputField num1;
+    public InputField num2;
+    public string answer;
+    public Text result;
+
+    public void PlusButton()
+    {
+        math.mathOperator = "+";
+    }
+    public void MinusButtton()
+    {
+        math.mathOperator = "-";
+    }
+    public void MultiplyButton()
+    {
+        math.mathOperator = "*";
+    }
+    public void DivideButton()
+    {
+        math.mathOperator = "/";
+    }
+    public void EqualsButton()
+    {
+        print(math.mathOperator);
+        answer = num1 + math.mathOperator + num2;
+        print(answer);
+    }
+}
---
+++ Calculator/Assets/Scripts/MathOperators.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathOperators : MonoBehaviour {
+
+    public string mathOperator = "+";
+
+	void Start ()
+    {
+		
+	}
+
+}
