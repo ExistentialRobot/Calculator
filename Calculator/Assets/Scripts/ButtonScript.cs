e8df2033cd4ca40af6149ca7c317131b7ac67754
Unknown <mistergroo25@gmail.com>
2017-04-12 22:18:12 -0600

    Made My Code Shorter And Made the Calculator Pretty

---
+++ Calculator/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+    public CalcScript numButtons;
+    public string num;
+    public Text equation;
+
+    public void ValueChange()
+    {
+        switch (numButtons.math)
+        {
+            case true:
+                numButtons.valueY += num;
+                equation.text = numButtons.valueX + numButtons.mathOp + numButtons.valueY;
+                break;
+            default:
+                numButtons.valueX += num;
+                equation.text = numButtons.valueX;
+                break;
+        }
+    }
+    public void MathOperator()
+    {
+        numButtons.mathOp = num;
+        numButtons.math = true;
+        equation.text = numButtons.valueX + numButtons.mathOp;
+    }
+}
---
+++ Calculator/Assets/Scripts/CalcScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalcScript : MonoBehaviour {
+
+    public Text equationLine;
+    public string valueX;
+    public string valueY;
+    public string equation;
+    public string mathOp;
+    public float x;
+    public float y;
+    public float z;
+    public bool math = false;
+
+    public void EqualsButton()
+    {
+        if (math)
+        {
+            float.TryParse(valueX, out x);
+            float.TryParse(valueY, out y);
+            switch (mathOp)
+            {
+                case " + ":
+                    z = x + y;
+                    break;
+                case " - ":
+                    z = x - y;
+                    break;
+                case " * ":
+                    z = x * y;
+                    break;
+                case " / ":
+                    z = x / y;
+                    break;
+            }
+        }
+        print(z);
+        equationLine.text = valueX + mathOp + valueY + " = " + z.ToString();
+        valueX = null;
+        valueY = null;
+        math = false;
+    }
+    public void ClearButton()
+    {
+        valueX = null;
+        valueY = null;
+        mathOp = null;
+        math = false;
+        equationLine.text = "0";
+    }
+}
